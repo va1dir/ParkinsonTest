53fc1309cc89af805e563e358fa51450e19abcce
Unknown <valdir@live.com>
2018-04-27 02:49:41 -0300

    Added new test

---
+++ Assets/Scripts/Catch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Catch : MonoBehaviour {
+
+    public Button yourButton;
+
+    private float secondsCount = 0;
+
+    private int quantity = 0;
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+    void Update()
+    {
+        if (secondsCount <= PlayerPrefs.GetFloat("timerTest2"))
+            secondsCount += Time.deltaTime;
+        else
+        {
+            PlayerPrefs.SetInt("resultTest2",quantity);
+            SceneManager.LoadScene("Result2");
+        }
+    }
+
+    void TaskOnClick()
+    {
+        quantity++;
+        yourButton.transform.position = new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height), 0);
+    }
+}
--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -6,16 +6,27 @@ using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
-    public Button yourButton;
+    public Button test1;
+    public Button test2;
+
+    public InputField time;
 
     void Start()
     {
-        Button btn = yourButton.GetComponent<Button>();
-        btn.onClick.AddListener(TaskOnClick);
+        Button btn1 = test1.GetComponent<Button>();
+        Button btn2 = test2.GetComponent<Button>();
+        btn1.onClick.AddListener(TaskOnClick1);
+        btn2.onClick.AddListener(TaskOnClick2);
     }
 
-    void TaskOnClick()
+    void TaskOnClick1()
     {
         SceneManager.LoadScene("Test");
+    }
+
+    void TaskOnClick2()
+    {
+        PlayerPrefs.SetFloat("timerTest2", float.Parse(time.text));
+        SceneManager.LoadScene("Test2");
     }
 }
